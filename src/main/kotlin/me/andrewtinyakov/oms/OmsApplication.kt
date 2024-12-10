09bd9ed215e0f671d2b3255d46265ea630af2d2c
AndrewTinyakov <oker.2005@mail.ru>
2024-12-10 17:01:01 +0300

    Initial commit

---
+++ src/main/kotlin/me/andrewtinyakov/oms/OmsApplication.kt
@@ -0,0 +1,11 @@
+package me.andrewtinyakov.oms
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OmsApplication
+
+fun main(args: Array<String>) {
+    runApplication<OmsApplication>(*args)
+}
